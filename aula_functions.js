9528ee156557df9bb38147c2a8ab3b6fa563ea95
Marco Salles <marco.salles@geekie.com.br>
2020-05-04 21:22:12 -0300

    Aula de functions - 2020-04-30_22-30

---
+++ aula_functions.js
@@ -0,0 +1,57 @@
+
+function fazerCafe(acucar) {	
+	// console.log('Ferver a água');
+	// console.log('Moer o café');
+	// console.log('Por pó no filtro');
+	// console.log('Colocar o filtro no bule');
+	// console.log('Por a agua no filtro');
+	// console.log('Esperar filtrar');
+	console.log('Servir o café na caneca');
+
+	var caneca = 'Caneca de café';
+	if (acucar > 0) {
+		// console.log('Colocar acucar na caneca');
+		caneca = caneca + ' com acucar';
+	}
+	return caneca;
+}
+
+// var cafeDoMax = fazerCafe();
+
+// console.log(cafeDoMax);
+
+
+// function soma(n1, n2) {
+// 	console.log('executou');
+// 	return n1 + n2;
+// }
+
+// soma(2, resultado);
+// console.log();
+
+
+// function imprimeNomeE_DepoisJoao(jurubeba) {
+// 	console.log(jurubeba);
+// 	jurubeba = 'Joao';
+// 	console.log(jurubeba);
+// }
+
+// var nome = 'Max';
+// imprimeNomeE_DepoisJoao(nome);
+// console.log(nome);
+
+
+function pegaUsuario(nome) {
+	// pega usuario do banco
+	return nome;
+}
+
+var usuario1 = pegaUsuario(nome1);
+var usuario2 = pegaUsuario(nome2);
+
+function ordenaAlfabeticamente(u1, u2) {
+	// calcula  a ordem alfabetica
+	// return
+}
+
+ordenaAlfabeticamente(usuario1, usuario2);
