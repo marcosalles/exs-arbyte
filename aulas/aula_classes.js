bc649179b45ed013ca217907f25cec9c3b960fa1
Marco Salles <marco.salles@geekie.com.br>
2020-05-11 18:23:02 -0300

    Adicionar cabelo na pessoa

--- aulas/aula_classes.js
+++ aulas/aula_classes.js
@@ -1,10 +1,11 @@
 
 class Pessoa {
-	constructor(nome) {
+	constructor(nome, cabelo) {
 		this.nome = nome;
+		this.cabelo = cabelo;
 	}
 }
 
-var max = new Pessoa('Max');
-
+var max = new Pessoa('Max', 'Booooooa noite', 'nenhum');
 console.log(max.nome);
+console.log(max.cabelo);
