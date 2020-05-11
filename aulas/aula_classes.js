8c173df405ed6eaad5b04575edf7a0c7acf81bdc
Marco Salles <marco.salles@geekie.com.br>
2020-05-11 18:24:21 -0300

    Adicionar funcoes a pessoa

--- aulas/aula_classes.js
+++ aulas/aula_classes.js
@@ -4,8 +4,19 @@ class Pessoa {
 		this.nome = nome;
 		this.cabelo = cabelo;
 	}
+
+	andar() {
+		console.log(`${this.nome} andou.`);
+	}
+
+	fazerCafe() {
+		console.log(`${this.nome} fez café.`);
+		return 'café sem açucar';
+	}
 }
 
-var max = new Pessoa('Max', 'Booooooa noite', 'nenhum');
+var max = new Pessoa('Max', 'nenhum');
 console.log(max.nome);
 console.log(max.cabelo);
+max.andar();
+console.log(max.fazerCafe());
