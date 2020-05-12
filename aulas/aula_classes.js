1b73458409bcfc3da235c00d0efb406b6b85731d
Marco Salles <marco.salles@geekie.com.br>
2020-05-11 22:02:04 -0300

    Aula de classes + ex12.1

--- aulas/aula_classes.js
+++ aulas/aula_classes.js
@@ -1,7 +1,23 @@
 
+function criarPessoa(nome, boasVindas, idade) {
+	this.nome = nome;
+	this.boasVindas = boasVindas;
+	this.idade = idade;
+	this.altura = 1.63;
+	this.darBoasVindas = function() {
+		console.log(`${this.boasVindas}`);
+	}
+}
+
+var pessoa = new criarPessoa('Max', 'Boooooa noite', 33);
+console.log(pessoa);
+pessoa.darBoasVindas()
+// pessoa.altura;
+console.log(pessoa.altura);
+
 class Pessoa {
 	constructor(nome, boasVindas, cabelo) {
-		this.nome = nome;
+		this.abracadabra = nome;
 		this.boasVindas = boasVindas;
 		this.cabelo = cabelo;
 	}
@@ -11,36 +27,58 @@ class Pessoa {
 	}
 
 	andar() {
-		console.log(`${this.nome} andou.`);
+		console.log(`${this.abracadabra} andou.`);
 	}
 
 	fazerCafe() {
-		console.log(`${this.nome} fez café.`);
-		return `café sem açucar feito por ${this.nome}`;
+		console.log(`${this.abracadabra} fez café.`);
+		return `café sem açucar feito por ${this.abracadabra}`;
+	}
+
+	get getNome() {
+		return this.abracadabra;
+	}
+
+	get getCabelo() {
+		return this.cabelo;
+	}
+
+	set setNome(nome) {
+		this.abracadabra = nome;
 	}
 }
 
 var max = new Pessoa('Max', 'Booooooa noite', 'nenhum');
-console.log(max.nome);
-console.log(max.cabelo);
-max.andar();
-console.log(max.fazerCafe());
+max.setNome = 'Marco';
+max.nome = 'Max';
+// console.log(max.getCabelo);
+console.log(max.getNome);
 
-var rafa = new Pessoa('Rafa', 'Coé', 'comprido');
-console.log(rafa.nome);
-console.log(rafa.cabelo);
-rafa.andar();
+// var objeto = {
+// 	nome: 'Max',
+// 	cabelo: 'nenhum'
+// };
 
-max.darBoasVindasPara(rafa);
-rafa.darBoasVindasPara(max);
+// console.log(max.nome);
+// console.log(max.cabelo);
+// max.andar();
+// console.log(max.fazerCafe());
 
-console.log(max.fazerCafe());
-max.nome = 'Marco';
-console.log(max.fazerCafe());
+// var rafa = new Pessoa('Rafa', 'Coé', 'comprido');
+// console.log(rafa.nome);
+// console.log(rafa.cabelo);
+// rafa.andar();
 
-console.log(rafa.fazerCafe());
-rafa.fazerCafe = function() {
-	console.log(`${this.nome} fez nescafé.`);
-	return `nescafé aguado feito por ${this.nome}`;
-}
-console.log(rafa.fazerCafe());
+// max.darBoasVindasPara(rafa);
+// rafa.darBoasVindasPara(max);
+
+// console.log(max.fazerCafe());
+// max.nome = 'Marco';
+// console.log(max.fazerCafe());
+
+// console.log(rafa.fazerCafe());
+// rafa.fazerCafe = function() {
+// 	console.log(`${this.nome} fez nescafé.`);
+// 	return `nescafé aguado feito por ${this.nome}`;
+// }
+// console.log(rafa.fazerCafe());
